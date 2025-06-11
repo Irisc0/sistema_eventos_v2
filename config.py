b8cd651c5234b6dc8205aff8c768025c74d3877c
Irisc0 <i17220636.19@puebla.tecnm.mx>
2025-06-11 09:33:03 -0600

    modificacion de la liga de la bd

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'clave_secreta')
+    
     SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/eventos.db'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
